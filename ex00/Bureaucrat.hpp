8085378097113268bf336ad5f52ac546211a6562
amdouyah <aminedouyahia00@gmail.com>
2023-12-12 12:35:35 +0100

    push

--- ex00/Bureaucrat.hpp
+++ ex00/Bureaucrat.hpp
@@ -3,6 +3,27 @@
 
 #include <iostream>
 
+class Bureaucrat{
+	private:
+		const std::string name;
+		int grade;
+	public :
+		class GradeTooHighException : public std::exception{
+			public:
+				const char* what() const throw();
+		};
+		class GradeTooLowException : public std::exception{
+			public:
+				const char* what()const throw();
+		};
+		Bureaucrat();
+		Bureaucrat(int grade, const std::string name);
+		std::string getName() const;
+		int getGrade();
+		void increment();
+		void decrement();
 
+};
+std::ostream &operator << (std::ostream Cout, const Bureaucrat &Bureaucrat);
 
 #endif
